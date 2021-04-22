179a10c8c22ecf8ce8da1c9baccf0054e395a72c
danceinthedark <2215629628@qq.com>
2021-04-22 23:59:17 +0800

    Revert "Revert "校区导航""
    This reverts commit e2496551604d11b79a31f8be0e6c461a2960267e.

--- navigation-back/map/urls.py
+++ navigation-back/map/urls.py
@@ -4,10 +4,11 @@ from .views import *
 
 app_name = "map"
 urlpatterns = [
-    path('map/', map, name='map'),
+    path('map/', give_map, name='map'),
     path('navigation/', navigation, name='navigation'),
     path('search-dest/', search_dest, name='search_dest'),
     path('search-path/', search_path, name='search_path'),
+    path('search-path-campus/', search_path_campus, name='search_path_campus'),
     path('location/', location, name='location'),
     path('finish/', finish, name='finish'),
     path('log/', log, name='log'),
